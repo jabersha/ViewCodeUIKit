29e054ccbdd870f4e2bcae1d2a0e7bdf45bf2369
jabersha <jaber_sha@hotmail.com>
2021-12-08 02:17:10 -0300

    Add Label and constraints between label and button

--- ViewCodeUIKit/View1.swift
+++ ViewCodeUIKit/View1.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SnapKit
 
 final class View1: UIView{
     
@@ -14,7 +13,14 @@ final class View1: UIView{
         let view = UIButton(frame: .zero)
         view.backgroundColor = .gray
         view.setTitle("Teste", for: .normal)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addTarget(self, action: #selector(buttonAct), for: .touchUpInside)
+        return view
+    }()
+    
+    lazy var label : UILabel = {
+        let view = UILabel(frame: .zero)
+        view.textAlignment = .center
+        view.backgroundColor = .white
         return view
     }()
     
@@ -26,21 +32,37 @@ final class View1: UIView{
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    @objc func buttonAct(sender: UIButton!){
+        print("Som de peido RISOS RISOS")
+    }
 }
 
 extension View1: CodeView{
     func buildHierarchy() {
+        addSubview(label)
         addSubview(button)
     }
     
     func configConstraints() {
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:  15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        label.snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(150)
+            make.topMargin.equalToSuperview().offset(20)
+            make.left.right.equalToSuperview().offset(15).inset(15)
+            make.bottomMargin.greaterThanOrEqualTo(button.snp.top)
+        }
+        button.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalTo(60)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(label.snp.bottom).offset(20)
+            make.bottomMargin.equalToSuperview().inset(50)
+        }
     }
     
     func additionalConfig() {
+        label.text = "RONALDO BRILHA MUITO NO CURINTIA"
         button.setTitle("AAAA", for: .normal)
         backgroundColor = .blue
 
